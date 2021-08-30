bc49dbeecf583b0fed57e5f88960281b9ee665f4
Sergiu A. Rus <rus.sergiu.adrian@gmail.com>
2021-08-30 21:56:59 +0300

    most recent posts on home page

--- source/index.blade.php
+++ source/index.blade.php
@@ -38,3 +38,18 @@
   <p><i>Coregraf de cuvinte</i></p>
 
   @endsection
+
+  @section('content')
+      <h1>Posts</h1>
+
+      <ul>
+          @forelse ($posts->sortBy('title') as $post)
+              <li>
+                  <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
+                  <small>{{ $post->prettyDate() }}</small>
+              </li>
+          @empty
+              <p>No posts to show.</p>
+          @endforelse
+      </ul>
+  @endsection
