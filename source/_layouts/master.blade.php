26f5bff93e1dfb3b2c6c8b1eebe82244861a62a4
Sergiu A. Rus <rus.sergiu.adrian@gmail.com>
2021-08-31 21:20:24 +0300

    31.10.1 links updated from home page

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -23,8 +23,8 @@
                 <strong>{{ $page->site->title }}</strong><br>
                 <ul>
                     <li><a href="/">Acasă</a></li>
-                    <li><a href="/posts">Articole</a></li>
-                    <li><a href="/about">Cine sunt?</a></li>
+                    <li><a href="/articole">Articole</a></li>
+                    <li><a href="/despre-mine">Cine sunt?</a></li>
                     <li><a href="/consultanta">Consultanță</a></li>
                     <li><a href="/contact">Țrr</a></li>
                 </ul>
