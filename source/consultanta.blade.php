bb942528accf53b8675cf9f8abb00f0389a4f0f0
Sergiu A. Rus <rus.sergiu.adrian@gmail.com>
2021-08-31 22:30:17 +0300

    31.12.5 1500

--- source/consultanta.blade.php
+++ source/consultanta.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <h3 style="text-align:center;" >
-<iframe frameborder="0" style="height:1550px;width:89%;border:none;" src='https://forms.zohopublic.eu/rus/form/Consultan/formperma/rIioBjyLRsyblo6y3t5ccuukOP6EDRH7_Wt6zMCWvl4'></iframe>
+<iframe frameborder="0" style="height:1500px;width:89%;border:none;" src='https://forms.zohopublic.eu/rus/form/Consultan/formperma/rIioBjyLRsyblo6y3t5ccuukOP6EDRH7_Wt6zMCWvl4'></iframe>
 </h3>
 
 @endsection
