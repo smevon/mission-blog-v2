127bc7a4137d19212ed24d5cf8006509a71f1898
Sergiu A. Rus <rus.sergiu.adrian@gmail.com>
2021-08-31 21:08:10 +0300

    31.8.2 one more try

--- source/index.blade.php
+++ source/index.blade.php
@@ -24,21 +24,24 @@
       <div id="mc_embed_signup_scroll">
   	<h2>Abonează-te</h2>
   <div class="indicates-required"><span class="asterisk">*</span> Musai</div>
+
   <div class="mc-field-group">
   	<label for="mce-EMAIL">Adresă de email <span class="asterisk">*</span>
   </label>
   	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
   </div>
+
   <div class="mc-field-group">
   	<label for="mce-FNAME">Prenumele tău preferat </label>
   	<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
   </div>
+
   	<div id="mce-responses" class="clear">
   		<div class="response" id="mce-error-response" style="display:none"></div>
   		<div class="response" id="mce-success-response" style="display:none"></div>
   	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-      <div style="position: absolute; left: -5000px; background-color:#A80874" aria-hidden="true"><input type="text" name="b_8afc636eecb08cf316417c49f_fd48a45d07" tabindex="-1" value=""></div>
-      <div class="clear"><input type="submit" value="Mă abonez" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+      <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8afc636eecb08cf316417c49f_fd48a45d07" tabindex="-1" value=""></div>
+      <div class="clear"><input style="background-color:#A80874" type="submit" value="Mă abonez" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
       </div>
   </form>
   </div>
