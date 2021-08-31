401c0a42b8a80d8e2b8f55231c732720bfbe3c67
Sergiu A. Rus <rus.sergiu.adrian@gmail.com>
2021-08-31 21:05:22 +0300

    31.8.1 same

--- source/index.blade.php
+++ source/index.blade.php
@@ -37,8 +37,8 @@
   		<div class="response" id="mce-error-response" style="display:none"></div>
   		<div class="response" id="mce-success-response" style="display:none"></div>
   	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-      <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8afc636eecb08cf316417c49f_fd48a45d07" tabindex="-1" value=""></div>
-      <div class="clear"><input type="submit" value="Mă abonez" name="subscribe" id="mc-embedded-subscribe" class="button" style="background-color:#A80874"></div>
+      <div style="position: absolute; left: -5000px; background-color:#A80874" aria-hidden="true"><input type="text" name="b_8afc636eecb08cf316417c49f_fd48a45d07" tabindex="-1" value=""></div>
+      <div class="clear"><input type="submit" value="Mă abonez" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
       </div>
   </form>
   </div>
