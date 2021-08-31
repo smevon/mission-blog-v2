3e9605062febf405e11c8e4ae4fc45c2564f75ae
Sergiu A. Rus <rus.sergiu.adrian@gmail.com>
2021-08-31 20:50:40 +0300

    31.3

--- source/index.blade.php
+++ source/index.blade.php
@@ -11,6 +11,21 @@
 
     <p>Ei bine, ăsta-i scopul meu aici, să-ți fie așezată ciorba mai frumos pe masă.</p>
 
+<!-- Begin Mailchimp Signup Form -->
+    <style>
+    input[type=button], input[type=submit], input[type=reset] {
+      background-color: #A80874;
+      border: none;
+      color: white;
+      padding: 16px 32px;
+      text-decoration: none;
+      margin: 4px 2px;
+      cursor: pointer;
+    }
+    input[type=button]:hover, input[type=submit]:hover, input[type=reset]:hover {
+      background-color: #A80874;
+    }
+
 
   <!-- Begin Mailchimp Signup Form -->
   <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
@@ -38,7 +53,7 @@
   		<div class="response" id="mce-success-response" style="display:none"></div>
   	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
       <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8afc636eecb08cf316417c49f_fd48a45d07" tabindex="-1" value=""></div>
-      <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+      <div class="clear"><input type="submit" value="Mă abonez" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
       </div>
   </form>
   </div>
