2fac9916cd584041e80d922e0d03ad40f1c36684
Sergiu A. Rus <rus.sergiu.adrian@gmail.com>
2021-09-02 21:30:09 +0300

    2.5 removed border for all images

--- source/despre-mine.blade.php
+++ source/despre-mine.blade.php
@@ -35,12 +35,12 @@
     <p>Așa că, iată-mă.</p>
 
     <style>
-    img {
+    .signature {
       border: 2px solid #A80874;
     }
     </style>
 
-    <img src="https://res.cloudinary.com/sergiu-adrian-ro/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:262c35/v1630434579/sergiu_udm9rd.jpg" alt="Sergiu Adrian, Coregraf de cuvinte." style="width:200px; border-radius:50%">
+    <img src="https://res.cloudinary.com/sergiu-adrian-ro/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:262c35/v1630434579/sergiu_udm9rd.jpg" alt="Sergiu Adrian, Coregraf de cuvinte." style="width:200px; border-radius:50%" class="signature">
 
 
     <p>Sunt Sergiu Adrian,</br>
