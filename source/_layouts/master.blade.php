186fc9b59b415e4ef66c464705c7a4b7be6106e6
Sergiu A. Rus <rus.sergiu.adrian@gmail.com>
2021-09-03 20:18:23 +0300

    3 1 trying navbar

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -13,6 +13,25 @@
     @include('_partials.head.favicon')
     @include('_partials.head.meta')
     @include('_partials.cms.identity_widget')
+<script>
+
+$(function(){
+  var str = '#len'; //increment by 1 up to 1-nelemnts
+  $(document).ready(function(){
+    var i, stop;
+    i = 1;
+    stop = 4; //num elements
+    setInterval(function(){
+      if (i > stop){
+        return;
+      }
+      $('#len'+(i++)).toggleClass('bounce');
+    }, 500)
+  });
+});
+
+</script>
+
 
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
 </head>
@@ -20,6 +39,31 @@
 
     <section>
         <header>
+
+          <div class="container-fluid">
+            <nav class="navbar navbar-inverse">
+              <div class="container-fluid">
+                <ul class="nav navbar-nav">
+                  <li><a id="len1" class="hoverable" href="/articole">Articole</a></li>
+                  <li><a id="len2" class="hoverable" href="/despre-mine">Cine sunt?</a></li>
+                  <li><a id="len3" class="hoverable" href="/consultanta">Consultanță</a></li>
+                  <li><a id="len4" class="hoverable" href="/trr">Țrr</a></li>
+                </ul>
+              </div>
+            </nav>
+            <div id="what-the-hell-is-this">
+              <div class="page-title">
+                <h2>Coregraf de cuvinte</h2>
+                <p class="lead">
+                  Based on Hover.css, the goal of this pen
+                  is to create a simple navigation bar <br />
+                  that can be easily reused in both mobile and native displays. Mouse over the nav text for animation!
+                </p>
+              </div>
+            </div>
+          </div>
+
+
 
             <nav>
                   <div style="text-align: center;">
