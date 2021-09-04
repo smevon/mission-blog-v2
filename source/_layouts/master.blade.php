1e7de7ae453fcad34567489d78caeb239e15ed99
Sergiu A. Rus <rus.sergiu.adrian@gmail.com>
2021-09-04 13:15:36 +0300

    4.6.1 nav-bar center icon left others right

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -46,10 +46,10 @@
     <nav class="navbar navbar-inverse">
       <div class="container-fluid">
         <ul class="nav navbar-nav">
-          <li><a id="len1" class="hoverable" href="/"><img src="https://res.cloudinary.com/sergiu-adrian-ro/image/upload/c_scale,w_32/v1630605466/Logos/Lion%20Logo%20Sergiu%20Adrian.png"></a></li>
-          <li><a id="len2" class="hoverable" href="/articole">Articole</a></li>
-          <li><a id="len3" class="hoverable" href="/despre-mine">Cine sunt?</a></li>
-          <li><a id="len4" class="hoverable" href="/consultanta">Consultanță</a></li>
+          <li><a id="len1" class="hoverable" href="/" style="text-align: left;"><img src="https://res.cloudinary.com/sergiu-adrian-ro/image/upload/c_scale,w_32/v1630605466/Logos/Lion%20Logo%20Sergiu%20Adrian.png"></a></li>
+          <li><a id="len2" class="hoverable" href="/articole" style="text-align: right;">Articole</a></li>
+          <li><a id="len3" class="hoverable" href="/despre-mine" style="text-align: right;">Cine sunt?</a></li>
+          <li><a id="len4" class="hoverable" href="/consultanta" style="text-align: right;">Consultanță</a></li>
         </ul>
       </div>
     </nav>
