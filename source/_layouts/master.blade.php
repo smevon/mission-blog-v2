36117d66e22fdb37466df1fa1606333605ccde47
Sergiu A. Rus <rus.sergiu.adrian@gmail.com>
2021-09-13 21:16:03 +0300

    all good now 3

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -47,12 +47,12 @@
     <nav class="navbar navbar-inverse">
       <div class="container-fluid">
         <ul class="nav navbar-nav">
-          <li class="items"><a href="/"><img src="https://res.cloudinary.com/sergiu-adrian-ro/image/upload/c_scale,w_32/v1630605466/Logos/Lion%20Logo%20Sergiu%20Adrian.png"></a></li>
+          <li><a href="/"><img src="https://res.cloudinary.com/sergiu-adrian-ro/image/upload/c_scale,w_32/v1630605466/Logos/Lion%20Logo%20Sergiu%20Adrian.png"></a></li>
 
-          <button id="len1" class="button-navbar items bounce" style="vertical-align:middle" href="/articole"><span>Articole</span></button>
-          <button id="len2" class="button-navbar items bounce" style="vertical-align:middle" href="/despre-mine"><span>Cine sunt eu?</span></button>
-          <button id="len3" class="button-navbar items bounce" style="vertical-align:middle" href="/consultanta"><span>Consultanță</span></button>
-          <button id="len4" class="button-navbar items bounce" style="vertical-align:middle" href="/trr"><span>Țrr</span></button>
+          <button id="len1" class="button-navbar bounce" style="vertical-align:middle" href="/articole"><span>Articole</span></button>
+          <button id="len2" class="button-navbar bounce" style="vertical-align:middle" href="/despre-mine"><span>Cine sunt eu?</span></button>
+          <button id="len3" class="button-navbar bounce" style="vertical-align:middle" href="/consultanta"><span>Consultanță</span></button>
+          <button id="len4" class="button-navbar bounce" style="vertical-align:middle" href="/trr"><span>Țrr</span></button>
 
 
         </ul>
