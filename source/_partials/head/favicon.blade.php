3ebd0ab6bd1808727fa4988afaf8c5dbed225da5
Sergiu A. Rus <rus.sergiu.adrian@gmail.com>
2021-11-18 22:29:35 +0200

    favicon

--- source/_partials/head/favicon.blade.php
+++ source/_partials/head/favicon.blade.php
@@ -1,5 +1,5 @@
 <!-- favicon -->
 {{-- try out https://realfavicongenerator.net --}}
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon16.png">
 <!-- end favicon -->
